f2cc7c8bd79f5671b4f3aa3f31d366d02f0dfa69
Bhupendra <shahi23.bhupendra11@gmail.com>
2023-08-21 23:08:23 +0530

    navbar done

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+
+export {Hero, SpecialOffer, SuperQuality, Services, Subscribe, CustomerReviews, Footer, PopularProducts } 
